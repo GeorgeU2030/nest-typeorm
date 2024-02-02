e1d14de23f3ace28506bac0fbe09aa591880036a
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-02 12:45:42 -0500

    createuser_has_been_added

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { createUserDTO } from './dto/user-create.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private usersService: UsersService) { }
+
+    @Post()
+    createUser(@Body() user: createUserDTO) {
+        return this.usersService.createUser(user);
+    }
+}
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { createUserDTO } from './dto/user-create.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    createUser(user: createUserDTO) {
+        const newuser = this.userRepository.create(user);
+        this.userRepository.save(newuser);
+    }
+}
